13be07bc2cce94d8322c8239136755d12595e134
skyerzz <spammen67@gmail.com>
2017-02-21 21:41:57 +0100

    Update ChatHandler.java
    Did chathandler to work (i think)

--- src/main/java/uk/tjevans/guildtoggle/ChatHandler.java
+++ src/main/java/uk/tjevans/guildtoggle/ChatHandler.java
@@ -1,5 +1,19 @@
 package uk.tjevans.guildtoggle;
 
 public class ChatHandler {
+  
+      @EventHandler
+      public void checkChat(ClientChatReceivedEvent e){
+          if(e.type!=0){
+              return;
+          }
+          String chat = e.message.getFormattedText().replaceAll("\u00A7.", "");
+          if(!chat.startsWith("Guild > ")){
+              return;
+          }
+          if (GuildToggle.x==false){
+              e.setCanceled(true);
+          }
+      }
 
 }
