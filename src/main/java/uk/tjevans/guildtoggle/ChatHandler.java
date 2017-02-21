2440d6087000b9d4adfb6faae8161c1000995ee2
thomas evans <djtev@users.noreply.github.com>
2017-02-21 20:29:29 +0000

    Add files via upload

---
+++ src/main/java/uk/tjevans/guildtoggle/ChatHandler.java
@@ -0,0 +1,5 @@
+package uk.tjevans.guildtoggle;
+
+public class ChatHandler {
+
+}
---
+++ src/main/java/uk/tjevans/guildtoggle/ExampleMod.java
@@ -0,0 +1,29 @@
+package uk.tjevans.guildtoggle;
+
+import net.minecraft.command.ICommand;
+import net.minecraftforge.client.ClientCommandHandler;
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.fml.common.Mod.EventHandler;
+import net.minecraftforge.fml.common.event.FMLInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPostInitializationEvent;
+import net.minecraftforge.fml.common.event.FMLPreInitializationEvent;
+
+@Mod(modid = ExampleMod.MODID, version = ExampleMod.VERSION)
+public class ExampleMod
+{
+    public static final String MODID = "guildtoggle";
+    public static final String VERSION = "0.1";
+    
+    public void preinit(FMLPreInitializationEvent event){
+     }
+    
+    @EventHandler
+    public void init(FMLInitializationEvent event)
+    {
+    	System.out.println("Guild Toggle Enabled");
+    	ClientCommandHandler.instance.registerCommand((ICommand)new GuildToggle());
+    }
+    
+    public void postInit(FMLPostInitializationEvent e) {
+    }
+}
